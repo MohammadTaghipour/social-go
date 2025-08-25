7efa3f45e0ef08d7f3cf2ef075c370cbd24eb8d0
Mohammad Taghipour <taghipourmohammad7@gmail.com>
2025-08-25 11:42:55 +0330

    Impl repository pattern

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/MohammadTaghipour/social/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
 	config config
+	store  store.Storage
 }
 
 type config struct {
@@ -20,15 +22,10 @@ type config struct {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
-	// A good base middleware stack
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
-	// Set a timeout value on the request context (ctx), that will signal
-	// through ctx.Done() that the request has timed out and further
-	// processing should be stopped.
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,21 @@ import (
 	"log"
 
 	"github.com/MohammadTaghipour/social/internal/env"
+	"github.com/MohammadTaghipour/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+
+	store := store.NewStorage(nil) // TODO: pass a real db connection
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
